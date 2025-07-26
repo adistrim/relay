72b6f97ffd910fedb0b56ffcbf1ae8820fc64637
Aditya Raj <adistrim.dev@gmail.com>
2025-07-27 00:11:39 +0530

    finally added the api route

--- server/main.go
+++ server/main.go
@@ -10,10 +10,13 @@ import (
 func main() {
 	router := gin.Default()
 	
-	router.GET("/api/", routes.InitRouter)
+	// system routes
+	router.GET("/api", routes.InitRouter)
 	router.GET("/api/health", routes.HealthCheck)
 	
+	// URL routes
 	router.POST("/api/shorten", routes.CreateShortUrl)
+	router.GET("/:code", routes.Forward)
 	
 	router.Run(":" + config.ENV.Port)
 }
