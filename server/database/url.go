7512ad8074f1eec9b67ae3e3d4f120d45c91660d
Aditya Raj <adistrim.dev@gmail.com>
2025-07-27 00:10:43 +0530

    add geturl db function

--- server/database/url.go
+++ server/database/url.go
@@ -18,3 +18,16 @@ func SaveURL(pool *pgxpool.Pool, longURL string) (string, error) {
 
 	return shortCode, nil
 }
+
+func GetURL(pool *pgxpool.Pool, code string) (string, error) {
+	var longURL string
+
+	sqlStatement := `SELECT long_url FROM urls WHERE short_code = $1;`
+
+	err := pool.QueryRow(context.Background(), sqlStatement, code).Scan(&longURL)
+	if err != nil {
+		return "", err
+	}
+
+	return longURL, nil
+}
