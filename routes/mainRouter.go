0e29e4ffc5b31f8eb7c93c749bb8c348b450f86a
Aditya Raj <adistrim.dev@gmail.com>
2025-07-28 01:32:08 +0530

    feat: integrated general and strict ratelimiting

--- routes/mainRouter.go
+++ routes/mainRouter.go
@@ -2,21 +2,31 @@ package routes
 
 import (
 	"html/template"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"relay/middleware"
 	"relay/templates"
 )
 
-func MainRouter() *gin.Engine {
+func MainRouter(redisClient *redis.Client) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+
+	router.SetTrustedProxies(nil)
+	
+	generalRateLimiter := middleware.RateLimitMiddleware(redisClient, 30, 1*time.Minute)
+	strictRateLimiter := middleware.RateLimitMiddleware(redisClient, 10, 1*time.Minute)
+
+	router.Use(generalRateLimiter)
 
 	tmpl := template.Must(template.ParseFS(templates.TemplatesFS, "html/*"))
 	router.SetHTMLTemplate(tmpl)
 
 	router.GET("/api", InitRouter)
 	router.GET("/api/health", HealthCheck)
-	router.POST("/api/shorten", CreateShortUrl)
+	router.POST("/api/shorten", CreateShortUrl, strictRateLimiter)
 
 	router.GET("/", templates.ServeHomePage)
 	router.GET("/:code", Forward)
