529f4dff1eec0421c5d335eb58fe62e801ad0484
Aditya <98259698+adistrim@users.noreply.github.com>
2025-07-29 00:31:56 +0530

    Feat/url caching (#4)
    * feat: optimize GetURL with Redis cache
    
    * pass redis client to GetURL

--- services/getLongUrl.go
+++ services/getLongUrl.go
@@ -7,8 +7,9 @@ import (
 func GetLongUrl(code string) (string, error) {
 	
 	pool := database.GetInstance()
+	redisClient := database.GetRedisInstance()
 	
-	url, err := database.GetURL(pool, code)
+	url, err := database.GetURL(pool, redisClient, code)
 	if err != nil {
 		return "", err
 	}
