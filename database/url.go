93630d9734330c0b9db88382ba89184a08018045
Aditya Raj <adistrim.dev@gmail.com>
2025-07-29 00:29:05 +0530

    feat: optimize GetURL with Redis cache

--- database/url.go
+++ database/url.go
@@ -2,7 +2,10 @@ package database
 
 import (
 	"context"
+	"time"
+
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
 
@@ -19,15 +22,24 @@ func SaveURL(pool *pgxpool.Pool, longURL string) (string, error) {
 	return shortCode, nil
 }
 
-func GetURL(pool *pgxpool.Pool, code string) (string, error) {
+func GetURL(pool *pgxpool.Pool, redisClient *redis.Client, code string) (string, error) {
+	ctx := context.Background()
+	cacheKey := "url:" + code
 	var longURL string
+	var err error
+	
+	longURL, err = redisClient.Get(ctx, cacheKey).Result()
+	if err == nil {
+		return longURL, nil
+	}
 
 	sqlStatement := `SELECT long_url FROM urls WHERE short_code = $1;`
-
-	err := pool.QueryRow(context.Background(), sqlStatement, code).Scan(&longURL)
+	err = pool.QueryRow(ctx, sqlStatement, code).Scan(&longURL)
 	if err != nil {
 		return "", err
 	}
-
+	
+	redisClient.Set(ctx, cacheKey, longURL, 24*time.Hour).Err()
+	
 	return longURL, nil
 }
